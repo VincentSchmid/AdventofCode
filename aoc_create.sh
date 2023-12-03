495d9ba7fe4f1331b5b3d4319004df085bfbf7dd
Vincent <puterich@gmail.com>
2023-12-03 13:29:19 +0100

    update create scripts

--- aoc_create.sh
+++ aoc_create.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
 # Create folders and files for Advent of Code days
-# Usage: ./create_day.sh
+# Usage: ./aoc_create.sh
 
 for DAY_NUM in {1..25}; do
     DAY_DIR="days/day$(printf "%02d" $DAY_NUM)"
+    DAY_FILE="$DAY_DIR/day$(printf "%02d" $DAY_NUM).go"
+    TEST_FILE="$DAY_DIR/day$(printf "%02d" $DAY_NUM)_test.go"
+    BENCHMARK_FILE="$DAY_DIR/benchmark_test.go"
 
     mkdir -p $DAY_DIR
-    touch $DAY_DIR/day$(printf "%02d" $DAY_NUM).go
-    touch $DAY_DIR/day$(printf "%02d" $DAY_NUM)_test.go
-    touch $DAY_DIR/benchmark_test.go
     touch $DAY_DIR/input.txt
 
+    # Create or overwrite dayXX.go
     echo "package day$(printf "%02d" $DAY_NUM)
 
-func Run() {
-    // Day $(printf "%02d" $DAY_NUM) implementation
-}" > $DAY_DIR/day$(printf "%02d" $DAY_NUM).go
+import (
+    \"fmt\"
+    \"adventofcode/utils\"
+)
 
+func Problem01(lines []string) int {
+    return 0
+}
+
+func Problem02(lines []string) int {
+    return 0
+}
+
+func Run() {
+    lines, _ := utils.ReadInputFile(utils.GetInputFilePath($DAY_NUM))
+    fmt.Println(Problem01(lines))
+    fmt.Println(Problem02(lines))
+}" > $DAY_FILE
+
+    # Create or overwrite dayXX_test.go
     echo "package day$(printf "%02d" $DAY_NUM)
 
 import (
@@ -27,23 +44,30 @@ import (
 
 func TestDay$(printf "%02d" $DAY_NUM)(t *testing.T) {
     assert.True(t, true) // Replace with actual tests
-}" > $DAY_DIR/day$(printf "%02d" $DAY_NUM)_test.go
+}" > $TEST_FILE
 
+    # Create or overwrite benchmark_test.go
     echo "package day$(printf "%02d" $DAY_NUM)
 
-import \"testing\"
+import (
+    \"testing\"
+    \"adventofcode/utils\"
+)
 
 func BenchmarkProblem1Day$(printf "%02d" $DAY_NUM)(b *testing.B) {
+    lines, _ := utils.ReadInputFile(utils.GetInputFilePath($DAY_NUM))
+
     for i := 0; i < b.N; i++ {
-        // Benchmark code for Problem 1
+        Problem01(lines)
     }
 }
 
 func BenchmarkProblem2Day$(printf "%02d" $DAY_NUM)(b *testing.B) {
+    lines, _ := utils.ReadInputFile(utils.GetInputFilePath($DAY_NUM))
     for i := 0; i < b.N; i++ {
-        // Benchmark code for Problem 2
+        Problem02(lines)
     }
-}" > $DAY_DIR/benchmark_test.go
+}" > $BENCHMARK_FILE
 
-    echo "Day $(printf "%02d" $DAY_NUM) structure with input file and benchmarks for Problem 1 and 2 created."
+    echo "Day $(printf "%02d" $DAY_NUM) Go file, test file, and benchmark test file created or overwritten."
 done
